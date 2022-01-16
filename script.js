c384cfaeb4bfa2c7ea1e231a7b8c575a9e04027a
Aayush Basnet <aayushbasnet07@gmail.com>
2022-01-16 16:18:45 +0545

    js init

---
+++ script.js
@@ -0,0 +1,9 @@
+const rgbColor = document.querySelector('.rgbColor');
+const boxes = document.querySelectorAll('.box');
+const easyBtn = document.querySelector('.easyBtn');
+const hardBtn = document.querySelector('.hardBtn');
+const playBtn = document.querySelector('playBtn');
+
+easyBtn.addEventListener('click', function() {
+    
+})
