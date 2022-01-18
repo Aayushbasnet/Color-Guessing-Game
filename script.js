a0b32345cf169def9b008b499c2438fd9c7d9519
Aayush Basnet <aayushbasnet07@gmail.com>
2022-01-18 21:26:37 +0545

    easy and hard button

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const rgbColor = document.querySelector('.rgbColor');
+const rgbColor = document.querySelector('#rgbColor');
 const boxes = document.querySelectorAll('.box');
 const easyBtn = document.querySelector('.easyBtn');
 const hardBtn = document.querySelector('.hardBtn');
@@ -9,6 +9,7 @@ class ColorGame{
     constructor(boxCount){
         this.boxCount = boxCount;
         this.colors = [];
+        this.reset();
     }
 
     // resetting game
@@ -18,12 +19,12 @@ class ColorGame{
         boxes.forEach(box => {
             box.style.backgroundColor = "white";
         });
-        this.boxCount = 6;
+        this.boxCount = this.boxCount;
     }
 
     // new game
     newGame(){
-        
+       this.colorPicker(); 
     }
 
     // Generates random rgb color
@@ -40,22 +41,53 @@ class ColorGame{
             this.colors.push(this.rgbGenerator());
         }
         console.log(this.colors);
+        this.updateBoxBg();
         return this.colors;
     }
 
     // update bg-color in box
     updateBoxBg() {
-        
+        const pickedColor = this.colors[Math.floor(Math.random() * this.boxCount)];     // selects random color from this.colors
+        rgbColor.textContent = pickedColor;     // Displaying Question
+        gameStatus.textContent = 
+            `
+                Playing !!  
+            `;
+        var i = 0;
+        // assigning random color to each (3 or 6) boxes 
+        boxes.forEach(box => {
+            if(this.colors[i]){
+                box.style.backgroundColor = this.colors[i];
+                box.style.display = "block";
+            }else {
+                box.style.display = "none";
+            } 
+            i++;
+        });       
     }
 
 }
 
 const game = new ColorGame(6);
+// Easy button clicking
 easyBtn.addEventListener('click', function() {
-    const boxCount = 3;
-    const easyGame = new ColorGame(boxCount);
-    console.log(easyGame.boxCount);
-    console.log(easyGame.colorPicker());
-})
+    const boxCount = 3;     // box value
+    easyBtn.style.backgroundColor = "#BAFFB4";      // changing bg color of easy button on click
+    hardBtn.removeAttribute("style");     // removing bg color of hard button if any
+
+    const easyGame = new ColorGame(boxCount);       // creating colorgame object with box value 3
+    easyGame.colorPicker();
+});
+
+// Hard button clicking
+hardBtn.addEventListener('click', function() {
+    const boxCount = 6;     // box value
+    easyBtn.removeAttribute("style");      // changing bg color of easy button on click
+    hardBtn.style.backgroundColor = "#BAFFB4";     // removing bg color of hard button if any
+
+    const hardGame = new ColorGame(boxCount);       // creating colorgame object with box value 6
+    hardGame.colorPicker();
+});
+
 console.log(game.boxCount);
 console.log(game.colorPicker());
